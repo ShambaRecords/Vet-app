78b1a040b20eeec40733b0d6ff878428bc99f00d
Kalela <philipkalela@gmail.com>
2021-03-12 17:28:02 +0300

    splash screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,11 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:vet_app/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Home page navigates and loads', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(Root());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
-
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that our first page has your bookings writen
+    expect(find.text('Your Bookings'), findsOneWidget);
   });
 }
